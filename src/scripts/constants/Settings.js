6cbb3c8ce93f80e7ffbaf646aa7620fd59a0d965
ahmedre <ahmedre@gmail.com>
2015-06-22 15:07:59 -0700

    Modify server to be at http://api:3000.
    In this case, api should be defined in /etc/hosts to allow easily
    changing the backend.

--- src/scripts/constants/Settings.js
+++ src/scripts/constants/Settings.js
@@ -1,4 +1,5 @@
 export default  {
-  url: 'http://localhost:3000/',
-  // url: 'http://45.55.158.85/'
+  // url: 'http://localhost:3000/',
+  // url: 'http://45.55.158.85/',
+  url: 'http://api:3000/'
 };
