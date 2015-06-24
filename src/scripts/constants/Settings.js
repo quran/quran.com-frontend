249a338a8716abd6c9147876994d040dd4509ebe
mmahalwy <mmahalwy@gmail.com>
2015-06-23 23:38:24 -0700

    Travis should work now

--- src/scripts/constants/Settings.js
+++ src/scripts/constants/Settings.js
@@ -1,5 +1,5 @@
 export default  {
-  // url: 'http://localhost:3000/',
+  url: 'http://localhost:3000/',
   // url: 'http://45.55.158.85/',
-  url: process.env.NODE_ENV === 'production' ? 'http://api:3000/' : 'http://45.55.158.85/'
+  // url: process.env.NODE_ENV === 'production' ? 'http://api:3000/' : 'http://45.55.158.85/'
 };
