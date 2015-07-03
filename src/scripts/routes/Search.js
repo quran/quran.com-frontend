fe8793765e28c67a8664a6156a969a8786f67db2
mmahalwy <mmahalwy@gmail.com>
2015-07-02 23:24:02 -0700

    98175224 New pagination

--- src/scripts/routes/Search.js
+++ src/scripts/routes/Search.js
@@ -3,14 +3,25 @@ import SearchHeader from 'components/header/SearchHeader';
 import {connectToStores} from 'fluxible/addons';
 import AyahsStore from 'stores/AyahsStore';
 import AyahsList from 'components/surah/AyahsList';
-import Pagination from 'components/Pagination';
+import ReactPaginate from 'react-paginate';
+import {handleRoute, NavLink, navigateAction} from 'fluxible-router';
 
 class Search extends React.Component {
-  constructor(props) {
-    super(props);
+  constructor(props, context) {
+    super(props, context);
+  }
+
+  handlePageClick(data) {
+    this.context.executeAction(navigateAction, {
+      url: `/search?q=${this.props.currentRoute.get('query').get('q')}&p=${data.selected + 1}`,
+      method: 'GET'
+    });
   }
 
   render() {
+    let pageNum = Math.ceil(this.props.stats.total / this.props.stats.size),
+        currentPage = (parseInt(this.props.currentRoute.get('query').get('p')) - 1) || 0;
+
     return (
       <div className="index-page">
         <SearchHeader />
@@ -24,7 +35,18 @@ class Search extends React.Component {
                 <span className="colored"> {this.props.stats.query}</span>
               </div>
               <div className="col-md-6 text-right">
-                <Pagination hitsPerPage={this.props.stats.hits} totalHits={this.props.stats.total} />
+                <ReactPaginate previousLabel={<span aria-hidden="true"><i className="ss-icon ss-directleft"/></span>}
+                       nextLabel={<span aria-hidden="true"><i className="ss-icon ss-directright"/></span>}
+                       breakLabel={<li className="break"><a href="">...</a></li>}
+                       pageNum={pageNum}
+                       marginPagesDisplayed={2}
+                       pageRangeDisplayed={5}
+                       initialSelected={currentPage}
+                       clickCallback={this.handlePageClick.bind(this)}
+                       containerClassName={"pagination"}
+                       subContainerClassName={"pages pagination"}
+                       activeClass={"active"} />
+
               </div>
             </div>
           </div>
@@ -43,10 +65,17 @@ class Search extends React.Component {
   }
 }
 
+Search.contextTypes = {
+  executeAction: React.PropTypes.func.isRequired,
+  getStore: React.PropTypes.func.isRequired
+};
+
 Search = connectToStores(Search, [AyahsStore], (stores, props) => {
   return {
     stats: stores.AyahsStore.getSearchStats()
   };
 });
+
+Search = handleRoute(Search);
 
 export default Search;
