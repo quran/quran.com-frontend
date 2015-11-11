560452f6fcfa6da2189fe4792ffc5ae2e90b226d
mmahalwy <mohamed@peek.com>
2015-11-10 16:41:49 -0800

    More fixes

---
+++ newrelic.js
@@ -0,0 +1,24 @@
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name: ['quran.com-frontend'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key: '7a6f48886ba3a707b10955d8edcbd7efe98dc02f',
+  logging: {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level: 'info'
+  }
+}
