08d3e5f9b7fcca7a9e64517ce73a476b97309d30
mmahalwy <mohamed@peek.com>
2015-11-12 23:20:01 -0800

    Removed cache

--- server/routes.js
+++ server/routes.js
@@ -3,10 +3,6 @@ var request = require('superagent-promise')(require('superagent'), Promise);
 
 import ls from 'loopstacks';
 import * as Settings from 'constants/Settings';
-// Cache for the time being until we change this
-const apicache = require('apicache')
-const cache = apicache.options({ debug: true }).middleware;
-
 import debug from 'utils/Debug';
 
 export default function(server) {
@@ -15,18 +11,11 @@ export default function(server) {
     path: '/loopstacks'
   }));
 
-  server.get('/api/cache/index', function(req, res, next) {
-    return res.status(200).send(apicache.getIndex());
-  });
-  server.get('/api/cache/clear', function(req, res, next) {
-    return res.send(200, apicache.clear());
-  });
-
   server.get(/^\/(images|fonts)\/.*/, function(req, res) {
     res.redirect(301, '//quran-1f14.kxcdn.com' + req.path);
   });
 
-  server.all('/api/*', cache('60 minutes'), function(req, res) {
+  server.all('/api/*', function(req, res) {
     debug('api:API', `Request: ${req.url}`);
 
     request.get(`${Settings.api}/${req.url.substr(4)}`)
@@ -39,6 +28,4 @@ export default function(server) {
       return res.status(500).send(response);
     });
   });
-
-  // server.use('/auth', require('./auth'));
 };
