c13877c28b20bfca154624f95d12a40370e77d2f
mmahalwy <mohamed@peek.com>
2016-01-04 22:58:04 -0800

    Remove tests

--- tests/functional/pages/home.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  url: 'http://localhost:' + process.env.PORT + '/',
-  elements: {
-    title: '.container h1',
-    regionDropdown: '.navbar-nav li:first-child a',
-    popover: '.popover'
-  }
-};
