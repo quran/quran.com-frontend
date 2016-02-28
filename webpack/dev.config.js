48d4174484260efb13a275d72f0dc58cfb63f542
ahmedre <ahmedre@gmail.com>
2016-02-27 19:01:38 -0800

    Fix Dockerfile and otf errors for rewrite
    This fixes the development build and allows the prod built to be built
    via Docker (though, at the moment, the prod build is still not working).

--- webpack/dev.config.js
+++ webpack/dev.config.js
@@ -75,7 +75,8 @@ module.exports = {
       { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/octet-stream" },
       { test: /\.(eot|otf)(\?v=\d+\.\d+\.\d+)?$/, loader: "file" },
       { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=image/svg+xml" },
-      { test: webpackIsomorphicToolsPlugin.regular_expression('images'), loader: 'url-loader?limit=10240' }
+      { test: webpackIsomorphicToolsPlugin.regular_expression('images'), loader: 'url-loader?limit=10240' },
+      { test: /\.(png|woff|woff2|eot|ttf|svg|otf)$/, loader: 'url-loader?limit=100000' }
     ]
   },
   progress: true,
