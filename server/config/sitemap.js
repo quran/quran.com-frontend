f98377e36c1ceb207292878c32c480bf0f69115b
Mohamed El Mahallawy <mmahalwy@gmail.com>
2016-04-03 00:49:20 -0700

    sitemap everything

--- server/config/sitemap.js
+++ server/config/sitemap.js
@@ -1,35 +1,50 @@
 import sitemap from 'sitemap';
 
-const urls = [];
+import ApiClient from '../../src/helpers/ApiClient';
 
-Array.apply(null, {length: 114}).forEach((_, index) => {
-  const surahId = index;
-
-  urls.push({
-    url: `/${surahId}`,
-    changefreq: 'daily',
-    priority: 0.9
-  });
-});
-
-const xml = sitemap.createSitemap({
-  hostname: 'http://quran.com',
-  cacheTime: 600000,  // 600 sec cache period
-  urls: [
-    ...urls,
-    { url: '/about',  changefreq: 'monthly', priority: 0.3 },
-    { url: '/contactus',  changefreq: 'monthly', priority: 0.3 },
-    { url: '/contact',  changefreq: 'monthly', priority: 0.3 },
-    { url: '/donations',  changefreq: 'monthly', priority: 0.3 },
-    { url: '/contributions',  changefreq: 'monthly', priority: 0.3 },
-
-    { url: '/search',  changefreq: 'weekly',  priority: 0.7 }
-  ]
-});
 
 export default (server) => {
   server.get('/sitemap.xml', (req, res) => {
-    res.header('Content-Type', 'application/xml');
-    res.send(xml.toString());
+    const client = new ApiClient(req);
+    const urls = [];
+
+    client.get('/surahs').then(surahs => {
+      surahs.forEach(surah => {
+        Array.apply(null, {length: surah.ayat}).forEach((_, index) => {
+          const ayahId = index;
+
+          urls.push({
+            url: `/${surah.id}/${ayahId}`,
+            changefreq: 'weekly',
+            priority: 0.8
+          });
+        });
+
+        urls.push({
+          url: `/${surah.id}`,
+          changefreq: 'weekly',
+          priority: 0.9
+        });
+      });
+
+
+      const xml = sitemap.createSitemap({
+        hostname: 'http://quran.com',
+        cacheTime: 600000,  // 600 sec cache period
+        urls: [
+          ...urls,
+          { url: '/about',  changefreq: 'monthly', priority: 0.3 },
+          { url: '/contactus',  changefreq: 'monthly', priority: 0.3 },
+          { url: '/contact',  changefreq: 'monthly', priority: 0.3 },
+          { url: '/donations',  changefreq: 'monthly', priority: 0.3 },
+          { url: '/contributions',  changefreq: 'monthly', priority: 0.3 },
+
+          { url: '/search',  changefreq: 'weekly',  priority: 0.7 }
+        ]
+      });
+
+      res.header('Content-Type', 'application/xml');
+      res.send(xml.toString());
+    }).catch(err => console.trace(err));
   });
 }
