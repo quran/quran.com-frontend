d431a5ad801f827dad951d78a6cc31bb440248e8
Mohamed El Mahallawy <mmahalwy@gmail.com>
2016-04-06 00:04:14 -0700

    Meta tags update

---
+++ src/helpers/makeHeadTags.js
@@ -0,0 +1,39 @@
+export default function makeHeadTags({ title, description, url, image }) {
+  const tags = {
+    meta: []
+  };
+
+  if (title) {
+    tags.title = title;
+    tags.meta.push(
+      {name: 'title', content: title},
+      {property: 'og:title', content: title},
+      {name: 'twitter:title', content: title}
+    );
+  }
+
+  if (description) {
+    tags.description = description;
+    tags.meta.push(
+      {name: 'description', content: description},
+      {property: 'og:description', content: description},
+      {name: 'twitter:description', content: description}
+    );
+  }
+
+  if (url) {
+    tags.meta.push(
+      {property: 'og:url', content: url},
+      {name: 'twitter:url', content: url}
+    );
+  }
+
+  if (image) {
+    tags.meta.push(
+      {property: 'og:image', content: image},
+      {name: 'twitter:image', content: image}
+    );
+  }
+
+  return tags;
+}
