70fbbfd3e20fd50cb90994b00918ce68355c0c22
Abiodun Shuaib <abiodun0@users.noreply.github.com>
2016-06-16 00:57:31 +0100

    (WIP) Issue #248 (#336)
    Fixes #248 Out of range bug

--- src/containers/Surah/index.js
+++ src/containers/Surah/index.js
@@ -170,7 +170,13 @@ export default class Surah extends Component {
   state = {
     lazyLoading: false
   };
-
+  componentWillMount() {
+    const {params, surah, push } = this.props;
+      let start = parseInt(params.range.split('-')[0], 10);
+      if(start > surah.ayat || isNaN(start)){
+         return push('/error/invalid-ayah-range');
+      }
+  }
   componentDidMount() {
     if (__CLIENT__) {
       window.removeEventListener('scroll', this.handleNavbar, true);
--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import IndexRoute from 'react-router/lib/IndexRoute';
 import Route from 'react-router/lib/Route';
-
+import checkValidSurah from './utils/checkValidSurah';
 import App from './containers/App';
+
+
 
 export default () => {
   return (
@@ -16,10 +18,12 @@ export default () => {
 
       <Route path="/contact" getComponent={(nextState, cb) => System.import('./containers/Contact').then(module => cb(null, module))} />
       <Route path="/contactus" getComponent={(nextState, cb) => System.import('./containers/Contact').then(module => cb(null, module))} />
+      <Route path="/error/:errorKey" getComponent={(nextState, cb) => System.import('./containers/Error').then(module => cb(null, module))} />
 
       <Route path="/search" getComponent={(nextState, cb) => System.import('./containers/Search').then(module => cb(null, module))} />
 
-      <Route path="/:surahId(/:range)" getComponent={(nextState, cb) => System.import('./containers/Surah').then(module => cb(null, module)).catch(err => console.trace(err))} />
+      <Route path="/:surahId(/:range)" getComponent={(nextState, cb) => System.import('./containers/Surah').then(module => cb(null, module)).catch(err => console.trace(err))}
+      onEnter={checkValidSurah} />
     </Route>
   );
 }
