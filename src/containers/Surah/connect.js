f9a900fa7d05810157695de983dac1f57e4e96e0
Mohamed El Mahallawy <mmahalwy@gmail.com>
2016-06-19 03:30:27 -0700

    wip

---
+++ src/containers/Surah/connect.js
@@ -0,0 +1,59 @@
+import { isAllLoaded, loadAll, setCurrent as setCurrentSurah } from '../../redux/modules/surahs';
+import { clearCurrent, isLoaded, load as loadAyahs } from '../../redux/modules/ayahs';
+
+export const surahsConnect = ({ store: { getState, dispatch } }) => {
+  debug('component:Surah:surahsConnect', 'Init');
+
+  if (!isAllLoaded(getState())) {
+    debug('component:Surah:surahsConnect', 'Surahs not loaded');
+
+    return dispatch(loadAll());
+  }
+
+  return true;
+}
+
+export const ayahsConnect = ({ store: { dispatch, getState }, params }) => {
+  debug('component:Surah:ayahsConnect', 'Init');
+
+  const range = params.range;
+  const surahId = parseInt(params.surahId, 10);
+
+  let from;
+  let to;
+
+  if (range) {
+    if (range.includes('-')) {
+      [from, to] = range.split('-');
+    } else {
+      // Single ayah. For example /2/30
+      from = range;
+      to = parseInt(range, 10) + ayahRangeSize;
+    }
+
+    if (isNaN(from) || isNaN(to)) {
+      // Something wrong happened like /2/SOMETHING
+      // going to rescue by giving beginning of surah.
+      [from, to] = [1, ayahRangeSize];
+    }
+  } else {
+    [from, to] = [1, ayahRangeSize];
+  }
+
+  from = parseInt(from, 10);
+  to = parseInt(to, 10);
+
+  if (surahId !== getState().surahs.current) {
+    dispatch(setCurrentSurah(surahId));
+  }
+
+  if (!isLoaded(getState(), surahId, from, to)) {
+    debug('component:Surah:ayahsConnect', 'Not loaded');
+
+    dispatch(clearCurrent(surahId)); // In the case where you go to same surah but later ayahs.
+
+    return dispatch(loadAyahs(surahId, from, to, getState().options));
+  }
+
+  return true;
+}
