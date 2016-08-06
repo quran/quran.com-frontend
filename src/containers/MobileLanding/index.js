a705e29c88fdd0052fa1b0192c31a582f5065d1e
Mohamed El Mahallawy <mmahalwy@gmail.com>
2016-08-06 15:08:33 -0700

    Mobile page updates (#424)
    * Mobile page updates
    
    * smaller
    
    * fix

--- src/containers/MobileLanding/index.js
+++ src/containers/MobileLanding/index.js
@@ -1,17 +1,28 @@
 import React from 'react';
+import Helmet from 'react-helmet';
 
 import Grid from 'react-bootstrap/lib/Grid';
 import Row from 'react-bootstrap/lib/Row';
 import Col from 'react-bootstrap/lib/Col';
 import Button from 'react-bootstrap/lib/Button';
 
+import makeHeadTags from 'helpers/makeHeadTags';
+
 const styles = require('./style.scss');
 const apple = require('../../../static/images/apple-white.svg');
 const play = require('../../../static/images/play-store.svg');
-const phones = require('../../../static/images/mockup_desktop@2x.png');
+const phones = require('../../../static/images/mockup-desktop@2x.png');
 
 export default () => (
   <Grid className={`${styles.container} montserrat`}>
+    <Helmet
+      {
+        ...makeHeadTags({
+          title: 'Quran Android and iOS apps by Quran.com',
+          description: 'From the makers of Quran.com comes Quran for iOS and Android, a beautiful, and ad-free mushaf apps' // eslint-disable-line max-len
+        })
+      }
+    />
     <Row>
       <Col md={5}>
         <h1 className={styles.title}>
@@ -23,7 +34,7 @@ export default () => (
             From the makers of Quran.com comes Quran for iOS, a beautiful,{' '}
             and ad-free mushaf app.
             It’s now easier to read the Quran on the go, memorize it and listen to your{' '}
-            favorite reciters this Ramadan.
+            favorite reciters.
           </small>
         </h3>
         <Row style={{paddingTop: 15}}>
