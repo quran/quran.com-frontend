0684cc018a7c2db6e7e1d0b81eb5ee05c82c3abf
Mohamed El Mahallawy <mmahalwy@gmail.com>
2016-08-13 18:49:13 -0700

    Kick

--- bin/server.prod.js
+++ bin/server.prod.js
@@ -2,14 +2,13 @@ require('dotenv').load();
 require('app-module-path').addPath(__dirname);
 require('app-module-path').addPath('./src');
 
+var webpackIsomorphicTools = require('webpack-isomorphic-tools');
 var path = require('path');
 var rootDir = path.resolve(__dirname, '../dist');
 
 global.__CLIENT__ = false;
 global.__SERVER__ = true;
 global.__DEVELOPMENT__ = process.env.NODE_ENV !== 'production';
-
-var webpackIsomorphicTools = require('webpack-isomorphic-tools');
 
 global.webpack_isomorphic_tools = new webpackIsomorphicTools(require('../webpack/isomorphic-tools-configuration'))
 .development(__DEVELOPMENT__)
