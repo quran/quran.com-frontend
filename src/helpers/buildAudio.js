35ede186892811d84de5b24db056b52520aee9b0
Ahmed El-Helw <ahmedre@gmail.com>
2016-08-17 01:23:15 -0700

    Simplify audio and segments building (#451)
    Simplify audio and segments building
    
    This patch goes with https://github.com/quran/quran.com-api/pull/73,
    which removes oggs and simplifies the response from the server.

--- src/helpers/buildAudio.js
+++ src/helpers/buildAudio.js
@@ -1,69 +1,22 @@
-export function testIfSupported(ayah, agent) {
-  const { audio } = ayah;
-
-  const testOperaOrFirefox = (agent.isOpera || agent.isFirefox);
-  const testChrome = agent.isChrome;
-
-  if (!audio) {
-    return false;
-  }
-
-  if (testOperaOrFirefox) {
-    if (!(audio.ogg && audio.ogg.url)) {
-      return false;
-    }
-  } else {
-    if (audio.mp3 && audio.mp3.url) {
-      return true;
-    } else if (audio.ogg && audio.ogg.url) {
-      if (!testChrome) {
-        return false;
-      }
-    } else {
-      return false;
-    }
-  }
-
-  return true;
-}
-
-export function buildAudioForAyah(audio, agent) {
+export function buildAudioForAyah(audio) {
   const scopedAudio = new Audio();
   let segments = null;
 
   scopedAudio.preload = 'none';
-
-  const testOperaOrFirefox = (agent.isOpera || agent.isFirefox);
-  const testChrome = agent.isChrome;
-
-
-  if (testOperaOrFirefox) {
-    if (audio.ogg && audio.ogg.url) {
-      scopedAudio.src = audio.ogg.url;
-      segments = audio.ogg.encryptedSegments;
-    }
-  } else {
-    if (audio.mp3 && audio.mp3.url) {
-      scopedAudio.src = audio.mp3.url;
-      segments = audio.mp3.encryptedSegments;
-    } else if (audio.ogg && audio.ogg.url) {
-      if (testChrome) {
-        scopedAudio.src = audio.ogg.url;
-        segments = audio.ogg.encryptedSegments;
-      }
-    }
+  if (audio.url) {
+    scopedAudio.src = audio.url;
+    segments = audio.encryptedSegments;
   }
-
 
   return { audio: scopedAudio, segments };
 }
 
-export function buildAudioFromHash(ayahsObject = {}, agent) {
+export function buildAudioFromHash(ayahsObject = {}) {
   const audioFromHash = {files: {}, segments: {}};
 
   Object.keys(ayahsObject).forEach(ayahId => {
     const ayah = ayahsObject[ayahId];
-    const audioForAyah = buildAudioForAyah(ayah.audio, agent);
+    const audioForAyah = buildAudioForAyah(ayah.audio);
 
     audioFromHash.files[ayahId] = audioForAyah.audio;
     audioFromHash.segments[ayahId] = audioForAyah.segments;
