61d999d313985f4ef50615890c988408de64f7d1
Sabeur Thabti <thabti.sabeur@gmail.com>
2016-08-21 14:51:00 +0100

    fix some of the suggestions

--- src/components/TopOptions/index.js
+++ src/components/TopOptions/index.js
@@ -8,45 +8,42 @@ import TooltipDropdown from 'components/TooltipDropdown';
 import ReadingModeToggle from 'components/ReadingModeToggle';
 import Share from 'components/Share';
 
-const TopOptions = ({options, surah, actions}) => {
-
-  return (
-    <Row>
-      <Col md={6} mdOffset={6} className="text-right">
-        <ul className="list-inline">
-          <li>
-            <InformationToggle
-              onToggle={actions.options.setOption}
-              isShowingSurahInfo={options.isShowingSurahInfo}
-            />
-          </li>
-          <li>|</li>
-          <li>
-            <FontSizeDropdown
-              options={options}
-              onOptionChange={actions.options.setOption}
-            />
-          </li>
-          <li>|</li>
-          <li>
-            <TooltipDropdown
-              options={options}
-              onOptionChange={actions.options.setOption}
-            />
-          </li>
-          <li>|</li>
-          <li>
-            <ReadingModeToggle
-              isToggled={options.isReadingMode}
-              onReadingModeToggle={actions.options.toggleReadingMode}
-            />
-          </li>
-          <li><Share surah={surah} /></li>
-        </ul>
-      </Col>
-    </Row>
-  );
-};
+const TopOptions = ({options, surah, actions}) => (
+  <Row>
+    <Col md={6} mdOffset={6} className="text-right">
+      <ul className="list-inline">
+        <li>
+          <InformationToggle
+            onToggle={actions.options.setOption}
+            isShowingSurahInfo={options.isShowingSurahInfo}
+          />
+        </li>
+        <li>|</li>
+        <li>
+          <FontSizeDropdown
+            options={options}
+            onOptionChange={actions.options.setOption}
+          />
+        </li>
+        <li>|</li>
+        <li>
+          <TooltipDropdown
+            options={options}
+            onOptionChange={actions.options.setOption}
+          />
+        </li>
+        <li>|</li>
+        <li>
+          <ReadingModeToggle
+            isToggled={options.isReadingMode}
+            onReadingModeToggle={actions.options.toggleReadingMode}
+          />
+        </li>
+        <li><Share surah={surah} /></li>
+      </ul>
+    </Col>
+  </Row>
+);
 
 TopOptions.propTypes = {
   options: PropTypes.object.isRequired,
