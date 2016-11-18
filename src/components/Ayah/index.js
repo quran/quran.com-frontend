a5108ddf0442abd64aefc1e94e5d533db331a89a
Arshad Khan <arshdkhn1@gmail.com>
2016-11-18 17:50:31 +0530

    tooltip fix

--- src/components/Ayah/index.js
+++ src/components/Ayah/index.js
@@ -37,6 +37,97 @@ export default class Ayah extends Component {
     isSearched: false
   };
 
+  componentDidMount() {
+    
+    function getOffset(elem) {
+      var offsetLeft = 0, offsetTop = 0;
+      do {
+        if ( !isNaN( elem.offsetLeft ) )
+        {
+          offsetLeft += elem.offsetLeft;
+          offsetTop += elem.offsetTop;
+        }
+      } while( elem = elem.offsetParent );
+      return {left: offsetLeft, top: offsetTop};
+    }
+
+    var targets = document.querySelectorAll( '[rel=tooltip]' ),
+    target  = false,
+    tooltip = false,
+    title   = false,
+    tip     = false;
+
+    for(var i = 0; i < targets.length; i++) {
+      targets[i].addEventListener("mouseenter", function() {
+        target  = this;
+        tip     = target.getAttribute("title");
+        tooltip = document.createElement("div");
+        tooltip.id = "tooltip";
+
+        if(!tip || tip == "")
+        return false;
+
+        target.removeAttribute("title");
+        tooltip.style.opacity = 0;
+        tooltip.innerHTML = tip;
+        document.body.appendChild(tooltip);
+
+        var init_tooltip = function()
+        {
+          console.log(getOffset(target));
+          // set width of tooltip to half of window width
+          if(window.innerWidth < tooltip.offsetWidth * 1.5)
+          tooltip.style.maxWidth = window.innerWidth / 2;
+          else
+          tooltip.style.maxWidth = 340;
+
+          var pos_left = getOffset(target).left + (target.offsetWidth / 2) - (tooltip.offsetWidth / 2),
+          pos_top  = getOffset(target).top - tooltip.offsetHeight - 10;
+          console.log("top is", pos_top);
+          if( pos_left < 0 )
+          {
+            pos_left = getOffset(target).left + target.offsetWidth / 2 - 20;
+            tooltip.classList.add("left");
+          }
+          else
+          tooltip.classList.remove("left");
+
+          if( pos_left + tooltip.offsetWidth > window.innerWidth )
+          {
+            pos_left = getOffset(target).left - tooltip.offsetWidth + target.offsetWidth / 2 + 20;
+            tooltip.classList.add("right");
+          }
+          else
+          tooltip.classList.remove("right");
+
+          if( pos_top < 0 )
+          {
+            var pos_top  = getOffset(target).top + target.offsetHeight + 15;
+            tooltip.classList.add("top");
+          }
+          else
+          tooltip.classList.remove("top");
+          // adding "px" is very important
+          tooltip.style.left = pos_left + "px";
+          tooltip.style.top = pos_top + "px";
+          tooltip.style.opacity  = 1;
+        };
+
+        init_tooltip();
+        window.addEventListener("resize", init_tooltip);
+
+        var remove_tooltip = function() {
+          tooltip.style.opacity  = 0;
+          document.querySelector("#tooltip") && document.body.removeChild(document.querySelector("#tooltip"));
+          target.setAttribute("title", tip );
+        };
+
+        target.addEventListener("mouseleave", remove_tooltip );
+        tooltip.addEventListener("click", remove_tooltip );
+      });
+    }
+  }
+
   shouldComponentUpdate(nextProps) {
     const conditions = [
       this.props.ayah !== nextProps.ayah,
@@ -150,22 +241,24 @@ export default class Ayah extends Component {
           <b
             key={word.code}
             id={id}
+            rel="tooltip"
             onClick={(event) => setCurrentWord(event.target.dataset.key)}
             data-key={`${word.ayahKey}:${position}`}
-            className={`${className} ${styles.Tooltip}`}
-            aria-label={tooltipContent}
+            className={`${className}`}
+            title={tooltipContent}
             dangerouslySetInnerHTML={{__html: word.code}}
           />
         );
       }
 
-      const label = isLast ? {'aria-label': `Verse ${ayah.ayahNum}`} : {}
+      const label = isLast ? {'title': `Verse ${ayah.ayahNum}`} : {}
       return (
         <b
           id={id}
           onClick={(event) => setCurrentWord(event.target.dataset.key)}
           data-key={`${word.ayahKey}:${position}`}
-          className={`${className} ${isLast && styles.Tooltip} pointer`}
+          rel="tooltip"
+          className={`${className} ${isLast} pointer`}
           key={word.code}
           dangerouslySetInnerHTML={{__html: word.code}}
           {...label}
