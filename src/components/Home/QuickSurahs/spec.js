f24e1c3486fab9e8299db08aef6daf8117d3891a
Arshad Khan <arshdkhn1@gmail.com>
2016-11-29 17:37:34 +0530

    corrected quicksurah tests

--- src/components/Home/QuickSurahs/spec.js
+++ src/components/Home/QuickSurahs/spec.js
@@ -4,18 +4,17 @@ import Link from 'react-router/lib/Link';
 import QuickSurahs from './index.js';
 
 describe("<QuickSurahs />", () => {
+  let clock;
 
   it("Should render QuickSurahs component", () => {
+    clock = sinon.useFakeTimers(1478975400000);
     let component = mount(<QuickSurahs />);
     expect(component).to.be.ok;
-    if (new Date().getDay() === 5)
-      expect(component.find('.list-inline li').length).to.equal(5);
-    else
-      expect(component.find('.list-inline li').length).to.equal(4);
+    expect(component.find('Link').length).to.equal(4);
   });
 
   it("Should render QuickSurahs component with Surah Al-Kahf", () => {
-    sinon.useFakeTimers(1470956400000);
+    clock = sinon.useFakeTimers(1478802600000);
     let component = mount(<QuickSurahs />);
     expect(component).to.be.ok;
     expect(component.find('a').length).to.equal(5);
