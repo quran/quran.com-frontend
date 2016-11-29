999c996a455d2dc6e71d231dbe4a098953c66e1d
Arshad Khan <arshdkhn1@gmail.com>
2016-11-29 17:48:07 +0530

    experimental check

--- src/components/Home/QuickSurahs/spec.js
+++ src/components/Home/QuickSurahs/spec.js
@@ -7,14 +7,18 @@ describe("<QuickSurahs />", () => {
   let clock;
 
   it("Should render QuickSurahs component", () => {
+    // Sat, nov 13 2016
     clock = sinon.useFakeTimers(1478975400000);
+    console.log("First Test date is -", new Date());
     let component = mount(<QuickSurahs />);
     expect(component).to.be.ok;
     expect(component.find('Link').length).to.equal(4);
   });
 
   it("Should render QuickSurahs component with Surah Al-Kahf", () => {
+    // Fri, nov 11 2016
     clock = sinon.useFakeTimers(1478802600000);
+    console.log("Second Test date is -", new Date());
     let component = mount(<QuickSurahs />);
     expect(component).to.be.ok;
     expect(component.find('a').length).to.equal(5);
