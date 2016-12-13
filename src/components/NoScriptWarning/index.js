ecd6c8a954a04442041c92c7f01ad9bd38db31c4
Naveed Ahmad <naveedahmada036@gmail.com>
2016-12-13 23:41:31 +0500

    minor css changes

--- src/components/NoScriptWarning/index.js
+++ src/components/NoScriptWarning/index.js
@@ -9,11 +9,11 @@ const styles = require('./style.scss');
 const NoScriptWarning = () => (
   <noscript>
     <Row>
-      <Col md={12} id={styles.noscript_warning}>
+      <Col md={12} className={styles.noscript_warning}>
         <p> Looks like either your browser does not support Javascript or its disabled. Quran.com workes best with JavaScript enabled.
           For more instruction on how to enable javascript
 
-          <Link to="http://www.enable-javascript.com/">Click here</Link>
+          <Link to="http://www.enable-javascript.com/"> Click here</Link>
         </p>
         </Col>
       </Row>
