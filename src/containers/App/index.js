dbfeeb870bbbd5bf3b1d2a4c587e686c8966483a
Naveed Ahmad <naveedahmada036@gmail.com>
2016-12-20 04:22:35 +0500

    fixed the link

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -50,7 +50,9 @@ class App extends Component {
             <Col md={12}>
               <p> Looks like either your browser does not support Javascript or its disabled. Quran.com workes best with JavaScript enabled.
                For more instruction on how to enable javascript
-               <Link to="http://www.enable-javascript.com/"> Click here </Link>
+                <a href="http://www.enable-javascript.com/">
+                  Click here
+                </a>
               </p>
             </Col>
           </Row>
