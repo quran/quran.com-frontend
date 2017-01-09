9fc52efe8dc674a75ef91d4ea87c4da8050c6929
Umair Idris <umair.m.idris@gmail.com>
2017-01-08 16:32:53 -0800

    Change resume heading to continue and support urdu/arabic translations

--- src/components/Home/LastVisit/index.js
+++ src/components/Home/LastVisit/index.js
@@ -9,7 +9,7 @@ const LastVisit = (props) => {
     <div className="">
       <h4 className={`text-muted ${styles.title}`}>
         <span>
-          Resume
+          <LocaleFormattedMessage id={'surah.index.continue'} defaultMessage={'Continue'}/>
           <Link to={`/${props.surah.id}/${props.ayah}`}>
             <span>
               {props.surah.name.simple} ({props.surah.id}:{props.ayah})
