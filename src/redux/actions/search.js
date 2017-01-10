a0122036ab06c38213c72759a3934e519eccc2fd
Mohamed El Mahallawy <mmahalwy@gmail.com>
2017-01-10 01:41:40 -0800

    Add new normalizr (#565)

--- src/redux/actions/search.js
+++ src/redux/actions/search.js
@@ -1,5 +1,4 @@
 import { ayahsSchema } from 'redux/schemas';
-import { arrayOf } from 'normalizr';
 
 import {
   SEARCH,
@@ -10,7 +9,7 @@ import {
 export function search(params) {
   return {
     types: [SEARCH, SEARCH_SUCCESS, SEARCH_FAIL],
-    schema: { results: arrayOf({ ayah: ayahsSchema }) },
+    schema: { results: [{ ayah: ayahsSchema }] },
     // TODO: We are doing this because of a weird obj.hasOwnProperty method missing on `params`
     promise: client => client.get('/v2/search', { params: { q: params.q } }),
     params
