f531d7b62146abe90142ec9036b3ad9a3404dcfc
Naveed Ahmad <naveedahmada036@gmail.com>
2017-01-11 21:21:53 +0500

    added optimized fonts for first two pages

--- src/helpers/buildFontFaces.js
+++ src/helpers/buildFontFaces.js
@@ -1,12 +1,18 @@
 /* eslint-disable max-len */
 
-export const fontFaceStyle = fontClassName => (
+/*export const fontFaceStyle = fontClassName => (
   `@font-face {font-family: '${fontClassName}';
   src: url('//quran-1f14.kxcdn.com/fonts/compressed/eot/${fontClassName}.eot?#iefix') format('embedded-opentype'),
   url('//quran-1f14.kxcdn.com/fonts/ttf/${fontClassName}.ttf') format('truetype'),
   url('//quran-1f14.kxcdn.com/fonts/woff/${fontClassName}.woff?-snx2rh') format('woff');}
   .${fontClassName} {font-family: '${fontClassName}';}
   .${fontClassName} {display: none;}`
+);*/
+
+
+
+export const fontFaceStyle = fontClassName => (
+  ``
 );
 
 export const fontFaceStyleLoaded = fontClassName => (
