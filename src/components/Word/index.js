54bdb1bb1a811448f13afe0163aff96392f895a0
Mohamed El Mahallawy <mmahalwy@gmail.com>
2017-01-11 22:39:54 -0800

    fix eslint

--- src/components/Word/index.js
+++ src/components/Word/index.js
@@ -1,29 +1,25 @@
 import React, { PropTypes } from 'react';
 
-const styles = require('../Ayah/style.scss');
-
 /* eslint-disable no-unused-vars */
 const CHAR_TYPE_WORD = 1;
 const CHAR_TYPE_END = 2;
 const CHAR_TYPE_PAUSE = 3;
 const CHAR_TYPE_RUB = 4;
 const CHAR_TYPE_SAJDAH = 5;
-/* eslint-enable no-unused-vars */
 
 export default class Line extends React.Component {
   static propTypes = {
-    word: PropTypes.object.isRequired,
+    word: PropTypes.object.isRequired, // eslint-disable-line
     tooltip: PropTypes.string,
-    audioActions: PropTypes.object.isRequired,
-    word: PropTypes.object.isRequired,
-    currentAyah: PropTypes.object.isRequired,
+    audioActions: PropTypes.object.isRequired, // eslint-disable-line
+    currentAyah: PropTypes.object.isRequired, // eslint-disable-line
     isPlaying: PropTypes.bool,
     isSearched: PropTypes.bool
   };
 
-  buildTooltip(word, tooltip){
+  buildTooltip = (word, tooltip) => {
     let title;
-    if (!word.wordId && word.charTypeId == CHAR_TYPE_END) {
+    if (!word.wordId && word.charTypeId === CHAR_TYPE_END) {
       title = `Verse ${word.ayahKey.split(':')[1]}`;
     } else {
       title = word[tooltip];
@@ -31,12 +27,13 @@ export default class Line extends React.Component {
     return title;
   }
 
-  handleWordClick(word){
+  handleWordClick = (word) => {
     const { currentAyah, audioActions, isPlaying, isSearched } = this.props;
-    if(isSearched) return;
-
-    if(currentAyah == word.ayahKey && isPlaying) {
-      audioActions.setCurrentWord(word.dataset.key) ;
+    if (isSearched) {
+      return;
+    }
+    if ((currentAyah === word.ayahKey) && isPlaying) {
+      audioActions.setCurrentWord(word.data.key);
     } else {
       audioActions.pause();
       audioActions.setAyah(word.dataset.ayah);
@@ -48,8 +45,8 @@ export default class Line extends React.Component {
     const { tooltip, word, currentAyah, isPlaying } = this.props;
 
     let id = null;
-    const  position = word.position - 1;
-    const highlight = currentAyah == word.ayahKey && isPlaying ? 'highlight' : '';
+    const position = word.position - 1;
+    const highlight = currentAyah === word.ayahKey && isPlaying ? 'highlight' : '';
     const className = `${word.className} ${highlight} ${word.highlight ? word.highlight : ''}`;
 
     if (word.charTypeId === CHAR_TYPE_WORD) {
@@ -57,16 +54,16 @@ export default class Line extends React.Component {
     }
 
     return (
-      <b
+      <b // eslint-disable-line
         key={word.code}
         id={id}
         rel="tooltip"
-        onClick={(event) => this.handleWordClick(event.target)}
+        onClick={event => this.handleWordClick(event.target)}
         data-key={`${word.ayahKey}:${position}`}
         data-ayah={word.ayahKey}
         className={`${className} pointer`}
         title={this.buildTooltip(word, tooltip)}
-        dangerouslySetInnerHTML={{__html: word.code}}
+        dangerouslySetInnerHTML={{ __html: word.code }}
       />
     );
   }
