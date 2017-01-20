ee5ae0558e3407f97f13524d21c39686999a6bda
Umair Idris <umair.m.idris@gmail.com>
2017-01-19 23:16:40 -0800

    Add translations for settings in sidebar

--- src/locale/en.js
+++ src/locale/en.js
@@ -61,6 +61,7 @@ export default {
     'actions.play': 'Play',
     'actions.pause': 'Pause',
 
+    'nav.settings': 'Settings',
     'nav.contribute': 'Contribute',
     'nav.aboutUs': 'About us',
     'nav.developers': 'Developers',
