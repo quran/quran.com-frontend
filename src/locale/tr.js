832ae9f0bed742f30ebb308c167a442ddb91992c
Mehmet <mehmetkalayci@users.noreply.github.com>
2017-02-28 01:38:34 +0200

    tr.js added
    Turkish language locale file added

---
+++ src/locale/tr.js
@@ -0,0 +1,81 @@
+// jscs:disable disallowQuotedKeysInObjects
+export default {
+  messages: {
+    local: 'turkish',
+    'local.siteLocale': 'Site Dili',
+    'local.changeLocal': 'Siteyi görüntülemek istediğiniz dili seçin ',
+    'local.navtiveName': 'Türkçe',
+    'local.selectLabel': 'Dil',
+    'setting.title': 'Ayarlar',
+    'setting.surahs': 'Sureler',
+    'setting.verses': 'Ayet\'e git',
+    'setting.reciters.title': 'Hafızlar',
+    'setting.translations.title': 'Çeviriler',
+    'setting.translations.removeAll': 'Tümünü kaldır',
+    'setting.translations.english': 'İngilizce',
+    'setting.translations.other': 'Diğer Diller',
+    'setting.fontSize': 'Yazı tipi boyutu',
+    'setting.fontSize.arabic': 'Arapça',
+    'setting.reading': 'Okuma',
+    'setting.tooltip': 'İpucu içeriği',
+    'setting.tooltip.title': 'İpucu görüntüleme',
+    'setting.tooltip.translation': 'Çeviri',
+    'setting.tooltip.transliteration': 'Harf çevirisi',
+    'setting.nightMode': 'Gece Modu',
+    'setting.nightMode.dayTip': 'Gündüz moduna geç',
+    'setting.nightMode.nightTip': 'Gece moduna geç',
+
+    'ayah.media.lectureFrom': 'Video anlatımı izle {from}',
+    'ayah.bookmarked': 'Yer işareti eklendi',
+    'ayah.bookmark': 'Yer işareti',
+
+    'search.placeholder': 'Arama "Nuh"',
+    'search.resultHeading': '{from}-{to} / <span> {total} </span> ARAMA SONUÇLARI: <span>{query}</span>', // eslint-disable-line max-len
+    'search.error': 'Maalesef aramanız ile ilgili bir hata oluştu.',
+    'search.nothing': 'Arama sorgusu yok.',
+    'search.noResult': 'Sonuç bulunamadı.',
+
+    'surah.next': 'SONRAKİ',
+    'surah.previous': 'ÖNCEKİ',
+    'surah.info': 'Sure Bilgisi',
+    'surah.index.heading': 'SURELER (BÖLÜMLER)',
+    'surah.index.continue': 'Devam',
+    'surah.index.quickLinks': 'Hızlı bağlantılar',
+    'surah.goToBeginning': 'Sure Başlangıcı',
+
+    'player.currentAyah': 'Ayet',
+    'player.nextAyah': 'Sonraki Ayet',
+    'player.previousAyah': 'Önceki Ayet',
+    'player.repeat.title': 'TEKRAR',
+    'player.repeat.single': 'Bir Kez',
+    'player.repeat.range': 'Aralık',
+    'player.repeat.rangeStart': 'Den',
+    'player.repeat.rangeEnd': 'E',
+    'player.repeat.times': 'Kez',
+    'player.repeat.loop': 'döngü',
+    'player.scrollButtonTip': 'Otomatik olarak çalmakta olan ayete git', // eslint-disable-line max-len
+
+    'app.loading': 'Yükleniyor...',
+    'actions.copy': 'Kopyala',
+    'actions.copied': 'Kopyalandı!',
+    'actions.play': 'Yürüt',
+    'actions.pause': 'Duraklat',
+
+    'nav.title': 'Kuran',
+    'nav.settings': 'Ayarlar',
+    'nav.contribute': 'Katkıda bulun',
+    'nav.aboutUs': 'Hakkımızda',
+    'nav.developers': 'Geliştiriciler',
+    'nav.usefulSites': 'Faydalı siteler',
+    'nav.otherLinks': 'Diğer bağlantılar',
+    'nav.contactUs': 'Bize ulaşın',
+    'nav.help': 'Yardım & geri bildirim',
+    'nav.aboutQuranProject': 'Quran.com (Kur\'an-ı Kerim) bir pro bono projesidir.', // eslint-disable-line max-len
+    'nav.rightsReserved': 'Tüm Hakları Saklıdır',
+    'nav.mobile': 'Mobil Uygulamalar',
+    'nav.navigate': 'Gezin', //there is no exact translation for Navigate in Turkish
+    'nav.legacySite': 'Eski site Quran.com',
+
+    'login.message': 'Tüm yer işaretlerinizi, notlarınızı ve etkinliklerinizi saklamak için Quran.com\'a giriş yapın.'
+  }
+};
