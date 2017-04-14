e89e5ed0182c3f514e36ecd8230144da48882304
Naveed Ahmad <naveedahmada036@gmail.com>
2017-04-15 02:51:48 +0500

    removed merge conflict

--- src/containers/Search/index.js
+++ src/containers/Search/index.js
@@ -13,37 +13,9 @@ import Loader from 'quran-components/lib/Loader';
 import { search } from 'redux/actions/search.js';
 import LocaleFormattedMessage from 'components/LocaleFormattedMessage';
 
-<<<<<<< HEAD
 const style = require('./style.scss');
 
 class Search extends Component {
-=======
-import { optionsType } from 'types';
-
-const style = require('./style.scss');
-
-class Search extends Component {
-  static propTypes = {
-    isErrored: PropTypes.bool,
-    isLoading: PropTypes.bool,
-    totalCount: PropTypes.number,
-    totalPages: PropTypes.number,
-    currentPage: PropTypes.number,
-    perPage: PropTypes.number,
-    query: PropTypes.string,
-    results: PropTypes.arrayOf(PropTypes.string),
-    push: PropTypes.func.isRequired,
-    location: PropTypes.shape({ // eslint-disable-line
-      q: PropTypes.string,
-      p: PropTypes.string
-    }),
-    options: optionsType
-  };
-
-  static defaultProps = {
-    results: []
-  };
->>>>>>> f060681... lint happy
 
   static contextTypes = {
     metrics: MetricsPropTypes.metrics
@@ -122,11 +94,7 @@ class Search extends Component {
   }
 
   renderBody() {
-    const { isErrored, isLoading, results, options, location: { query } } = this.props;
-
-    if (isLoading) {
-      return <Loader isActive={isLoading} />;
-    }
+    const { isErrored, isLoading, results, entities, options, location: { query } } = this.props;
 
     if (!query || !query.q) {
       return (
@@ -144,6 +112,10 @@ class Search extends Component {
       );
     }
 
+    if (isLoading) {
+      return <Loader isActive={isLoading} />;
+    }
+
     if (!results.length) {
       return (
         <h3 className="text-center" style={{ padding: '15%' }}>
@@ -154,9 +126,9 @@ class Search extends Component {
 
     return results.map(result => (
       <Verse
-        verse={result}
-        // match={entities[result].match}
-        key={result.verseKey}
+        verse={entities[result]}
+        match={entities[result].match}
+        key={entities[result].verseKey}
         tooltip={options.tooltip}
         userAgent={options.userAgent}
         isSearched
