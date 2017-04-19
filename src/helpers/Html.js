39ba4cf032b309304b4a7a2bc1f43c9927fc5f5a
Mohamed El Mahallawy <mmahalwy@gmail.com>
2017-04-19 00:45:04 -0700

    Google optimize flicker (#741)

--- src/helpers/Html.js
+++ src/helpers/Html.js
@@ -34,6 +34,20 @@ const Html = ({ store, component, assets }) => {
       </head>
       <body>
         <div id="app" dangerouslySetInnerHTML={{ __html: content }} />
+        <style
+          dangerouslySetInnerHTML={{
+            __html: '.async-hide { opacity: 0 !important}'
+          }}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(a,s,y,n,c,h,i,d,e){s.className+=' '+y;h.start=1*new Date;
+            h.end=i=function(){s.className=s.className.replace(RegExp(' ?'+y),'')};
+            (a[n]=a[n]||[]).hide=h;setTimeout(function(){i();h.end=null},c);h.timeout=c;
+            })(window,document.documentElement,'async-hide','dataLayer',4000,
+            {'GTM-PNMFTW3':true});`
+          }}
+        />
         <script
           dangerouslySetInnerHTML={{
             __html: `(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
