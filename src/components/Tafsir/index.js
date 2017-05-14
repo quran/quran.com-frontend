4b15a87a85c395194e63141ce916cb9dd75df14c
Naveed Ahmad <naveedahmada036@gmail.com>
2017-05-14 18:00:47 +0500

    right align verse text

--- src/components/Tafsir/index.js
+++ src/components/Tafsir/index.js
@@ -9,7 +9,7 @@ export default class Tafsir extends Component {
     return (
       <div className="col-md-10 col-md-offset-1">
         <h4 className="montserrat">{tafsir.resourceName}</h4>
-        <h2>{verse.textMadani}</h2>
+        <h2 className="text-right">{verse.textMadani}</h2>
         <p
           className={`${tafsir.languageName} text-right`}
           dangerouslySetInnerHTML={{ __html: tafsir.text }}
