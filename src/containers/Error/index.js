6aef5ef1163344f44fb826a99a67f491130cb781
Naveed Ahmad <naveedahmada036@gmail.com>
2017-05-23 15:35:37 +0500

    translated error page messages

--- src/containers/Error/index.js
+++ src/containers/Error/index.js
@@ -1,31 +1,25 @@
 import React, { PropTypes } from 'react';
 import IndexHeader from 'components/IndexHeader';
 import Helmet from 'react-helmet';
+import { FormattedHTMLMessage } from 'react-intl';
 
 const error = {
-  'invalid-surah': 'Surah is out of range',
-  'invalid-ayah': 'Ayah(s) selected are out of range'
+  'invalid-surah': "Surah is out of range. Please go to <a href='/'> home page</a> and select a Surah",
+  'invalid-ayah': "Ayah is out of range. Please go to <a href='/'> home page </a> and select a Surah/Ayah"
 };
 
 const ErrorPage = ({ params }) => (
   <div>
-    <Helmet title={`Error ${params.errorTitle}`} />
+    <Helmet title={`Error ${error[params.errorKey]}`} />
     <IndexHeader noSearch />
     <div className="container-fluid about-text">
       <div className="row">
         <div className="col-md-8 col-md-offset-2">
-          <h4 className="source-sans">
-            {error[params.errorKey]}
-            . Please go to the
-            {' '}
-            <a href="/">home page</a>
-            {' '}
-            and select a Surah/Ayay.
-            Please go to the
-            {' '}
-            <a href="/">home page</a>
-            {' '}
-            and select a Surah/Ayah
+          <h4 className="source-sans text-center">
+            <FormattedHTMLMessage
+              id="error.invalid-surah"
+              defaultMessage={error[params.errorKey]}
+            />
           </h4>
         </div>
       </div>
