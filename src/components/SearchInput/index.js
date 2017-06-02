085ec833f18eb5b94d2eead29b2dba63d0c098e2
Naveed Ahmad <naveedahmada036@gmail.com>
2017-06-02 05:27:42 +0500

    implemented basic vocie search #744

--- src/components/SearchInput/index.js
+++ src/components/SearchInput/index.js
@@ -9,14 +9,67 @@ import SearchAutocomplete from 'components/SearchAutocomplete';
 import debug from 'helpers/debug';
 
 class SearchInput extends Component {
-
   static contextTypes = {
     metrics: MetricsPropTypes.metrics
   };
 
   state = {
     value: '',
-    showAutocomplete: false
+    showAutocomplete: false,
+    listening: false,
+    voiceEnabled: false
+  };
+
+  componentDidMount() {
+    if (__CLIENT__) {
+      // eslint-disable-next-line react/no-did-mount-set-state
+      this.setState({ voiceEnabled: !!this.getSpeechRecognizer() });
+    }
+  }
+
+  shouldComponentUpdate(nextProps, newState) {
+    return [
+      this.state.voiceEnabled !== newState.voiceEnabled,
+      this.state.newState !== newState.showAutocomplete
+    ].some(test => test);
+  }
+
+  onSpeechResult = (event) => {
+    const input = this.input;
+    const text = event.results[0][0].transcript;
+
+    input.value = text;
+    this.setState({ value: text, showAutocomplete: true });
+  };
+
+  // eslint-disable-next-line no-unused-vars
+  onSpeechEnd = (event) => {
+    this.setState({ listening: false });
+    // TODO: search ?
+  };
+
+  // eslint-disable-next-line no-unused-vars
+  onSpeechError = (event) => {
+    // TODO: should we show err to user?
+  };
+
+  getSpeechRecognizer = () =>
+    window.SpeechRecognition || window.webkitSpeechRecognition;
+
+  initSpeechRecongination = () => {
+    if (this.state.listening) {
+      return false;
+    }
+    const recognizer = new (this.getSpeechRecognizer())();
+
+    recognizer.lang = 'ar';
+    recognizer.interimResults = true;
+    recognizer.onresult = this.onSpeechResult;
+    recognizer.onend = this.onSpeechEnd;
+
+    this.setState({ listening: true });
+    recognizer.start();
+    return false;
   };
 
   search = (event) => {
@@ -24,7 +77,10 @@ class SearchInput extends Component {
     const shortcutSearch = /\d[\.,\:,\,,\\,//]/g; // eslint-disable-line no-useless-escape
     const splitSearch = /[\.,\:,\,,\\,//]/g; // eslint-disable-line no-useless-escape
 
-    if (event.key === 'Enter' || event.keyCode === 13 || event.type === 'click') {
+    if (
+      event &&
+      (event.key === 'Enter' || event.keyCode === 13 || event.type === 'click')
+    ) {
       const inputEl = this.input;
       const searching = inputEl.value.trim();
       let ayah;
@@ -40,14 +96,8 @@ class SearchInput extends Component {
       const pattern = new RegExp(shortcutSearch);
 
       if (pattern.test(searching)) {
-        surah = parseInt(
-          searching.split(splitSearch)[0],
-          10
-        );
-        ayah = parseInt(
-          searching.split(splitSearch)[1],
-          10
-        );
+        surah = parseInt(searching.split(splitSearch)[0], 10);
+        ayah = parseInt(searching.split(splitSearch)[1], 10);
 
         if (isNaN(ayah)) {
           ayah = 1;
@@ -55,10 +105,10 @@ class SearchInput extends Component {
 
         this.context.metrics.track('Search', {
           action: 'surah',
-          label: `/${surah}/${ayah}-${(ayah + 10)}`
+          label: `/${surah}/${ayah}-${ayah + 10}`
         });
 
-        return this.props.push(`/${surah}/${ayah}-${(ayah + 10)}`);
+        return this.props.push(`/${surah}/${ayah}-${ayah + 10}`);
       }
 
       this.context.metrics.track('Search', {
@@ -82,30 +132,44 @@ class SearchInput extends Component {
     }
 
     return false;
+  };
+
+  renderVoiceSearch() {
+    return (
+      <a tabIndex="-1" onClick={this.initSpeechRecongination}>
+        <i className="ss-icon ss-mic" />
+      </a>
+    );
   }
 
   render() {
-    const { showAutocomplete } = this.state;
+    const { showAutocomplete, voiceEnabled } = this.state;
     const { className, intl } = this.props;
-    const placeholder = intl.formatMessage({ id: 'search.placeholder', defaultMessage: 'Search' });
+    const placeholder = intl.formatMessage({
+      id: 'search.placeholder',
+      defaultMessage: 'Search'
+    });
 
     debug('component:SearchInput', 'Render');
 
     return (
       <div className={`right-inner-addon searchinput ${className}`}>
-        <a tabIndex="-1" onClick={this.search}><i className="ss-icon ss-search" /></a>
+        <a tabIndex="-1" onClick={this.search}>
+          <i className="ss-icon ss-search" />
+        </a>
+        {voiceEnabled && this.renderVoiceSearch()}
         <input
           type="search"
           placeholder={placeholder}
-          ref={(input) => { this.input = input; }}
+          ref={(input) => {
+            this.input = input;
+          }}
           onFocus={() => this.setState({ showAutocomplete: true })}
           // onBlur={() => this.setState({ showAutocomplete: false })}
           onKeyUp={this.search}
         />
-        {
-          showAutocomplete &&
-            <SearchAutocomplete value={this.state.value} input={this.input} />
-        }
+        {showAutocomplete &&
+          <SearchAutocomplete value={this.state.value} input={this.input} />}
       </div>
     );
   }
