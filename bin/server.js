20be964394fe58b77aa7e21f8f1c23c5f0026034
Uman Shahzad <uman@opencraft.com>
2017-06-09 15:18:16 +0500

    Undo server.*.js changes

--- bin/server.js
+++ bin/server.js
@@ -2,40 +2,37 @@ require('dotenv').load();
 require('app-module-path').addPath(__dirname);
 require('app-module-path').addPath('../src');
 
-const server = require('../src/server.js');
-const fs = require('fs');
-const path = require('path');
-const piping = require('piping');
-const webpackIsomorphicTools = require('webpack-isomorphic-tools');
-const webpackIsomorphicToolsConfiguration = require('../webpack/isomorphic-tools-configuration');
-const babelRegister = require('babel-register');
-
-const babelrc = fs.readFileSync('./.babelrc');
-const rootDir = path.resolve(__dirname, '..');
+var fs = require('fs');
+var path = require('path');
+var webpackIsomorphicTools = require('webpack-isomorphic-tools');
+var babelrc = fs.readFileSync('./.babelrc');
+var rootDir = path.resolve(__dirname, '..');
+var config;
 
 try {
-  const config = JSON.parse(babelrc);
-  babelRegister(config);
+  config = JSON.parse(babelrc);
 } catch (err) {
   console.error('==>     ERROR: Error parsing your .babelrc.');
   console.error(err);
 }
+
+require('babel-register')(config);
 
 global.__CLIENT__ = false;
 global.__SERVER__ = true;
 global.__DEVELOPMENT__ = process.env.NODE_ENV !== 'production';
 
 if (__DEVELOPMENT__) {
-  if (!piping({
+  if (!require('piping')({
       hook: true,
       ignore: /(\/\.|~$|\.json|\.scss$)/i
-  })) {
+    })) {
     return;
   }
 }
 
-global.webpack_isomorphic_tools = new webpackIsomorphicTools(webpackIsomorphicToolsConfiguration)
-.development(process.env.NODE_ENV === 'development')
-.server(rootDir, () => {
-  server.default();
-});
+global.webpack_isomorphic_tools = new webpackIsomorphicTools(require('../webpack/isomorphic-tools-configuration'))
+  .development(process.env.NODE_ENV === 'development')
+  .server(rootDir, function() {
+    require('../src/server.js').default();
+  });
--- bin/server.prod.js
+++ bin/server.prod.js
@@ -2,19 +2,16 @@ require('dotenv').load();
 require('app-module-path').addPath(__dirname);
 require('app-module-path').addPath('../dist');
 
-var server = require('../dist/server.js');
-var WebpackIsomorphicTools = require('webpack-isomorphic-tools');
-var WebpackIsomorphicToolsConfiguration = require('../webpack/isomorphic-tools-configuration');
+var webpackIsomorphicTools = require('webpack-isomorphic-tools');
 var path = require('path');
-
 var rootDir = path.resolve(__dirname, '../dist');
 
 global.__CLIENT__ = false;
 global.__SERVER__ = true;
 global.__DEVELOPMENT__ = process.env.NODE_ENV !== 'production';
 
-global.webpack_isomorphic_tools = new WebpackIsomorphicTools(WebpackIsomorphicToolsConfiguration)
-.development(__DEVELOPMENT__)
-.server(rootDir, () => {
-  server.default();
-});
+global.webpack_isomorphic_tools = new webpackIsomorphicTools(require('../webpack/isomorphic-tools-configuration'))
+  .development(__DEVELOPMENT__)
+  .server(rootDir, function() {
+    require('../dist/server.js').default();
+  });
