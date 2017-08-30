ed813f02c689660ef2c599c55f843def8d716dff
Mohamed El Mahallawy <mmahalwy@gmail.com>
2017-08-29 22:54:21 -0700

    sidebar fix

--- src/components/GlobalSidebar/index.js
+++ src/components/GlobalSidebar/index.js
@@ -1,180 +1,48 @@
-/* global document */
-import styled from 'styled-components';
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
 import Icon from 'quran-components/lib/Icon';
-import Navbar from 'react-bootstrap/lib/Navbar';
 import Menu, { MenuItem } from 'quran-components/lib/Menu';
 import LocaleFormattedMessage from 'components/LocaleFormattedMessage';
 
-const NavbarHeader = Navbar.Header;
+const GlobalSidebar = () => (
+  <Menu>
+    <MenuItem
+      icon={<Icon type="help" />}
+      href="https://quran.zendesk.com/hc/en-us"
+    >
+      <LocaleFormattedMessage id="nav.help" defaultMessage="Help & feedback" />
+    </MenuItem>
+    <MenuItem icon={<Icon type="cell" />} href="/apps">
 
-const styles = require('./style.scss');
+      <LocaleFormattedMessage id="nav.mobile" defaultMessage="Mobile" />
 
-const List = styled.ul`
-  padding-left: 0;
-
-  li{
-    color: #777;
-
-    a{
-      color: #777;
-      padding: 10px 15px;
-      display: block;
-
-      .ss-icon{
-        font-size: 18px;
-        margin-right: 20px;
-      }
-
-      &:hover{
-        background: #f5f5f5;
-        color: #333;
-      }
-    }
-  }
-`;
-
-class GlobalSidebar extends Component {
-  state = {
-    settingsModalOpen: false
-  };
-
-  componentDidMount() {
-    document.body.addEventListener('click', this.onBodyClick.bind(this), true);
-  }
-
-  componentWillUnmount() {
-    document.body.removeEventListener(
-      'click',
-      this.onBodyClick.bind(this),
-      true
-    );
-  }
-
-  onBodyClick = () => {
-    const { open, handleOpen } = this.props;
-
-    if (open) {
-      return handleOpen(false);
-    }
-
-    return false;
-  };
-
-  render() {
-    const { open, handleOpen, children } = this.props;
-
-    return (
-      <div className={`${styles.container} sidebar ${open && styles.open}`}>
-        <Navbar static fluid>
-          <NavbarHeader>
-            <p // eslint-disable-line
-              className="navbar-text"
-              onClick={() => handleOpen(false)}
-            >
-              <Link to="/">
-                <i className={`ss-icon ss-home ${styles.backToHome}`} />
-                <LocaleFormattedMessage id="nav.title" defaultMessage="Quran" />
-              </Link>
-            </p>
-          </NavbarHeader>
-        </Navbar>
-        <List>
-          {children}
-          <li>
-            <a
-              href="https://quran.zendesk.com/hc/en-us"
-              data-metrics-event-name="Sidebar:Link:Help"
-            >
-              <i className="ss-icon ss-help vertical-align-middle" />{' '}
-              <LocaleFormattedMessage
-                id="nav.help"
-                defaultMessage="Help & feedback"
-              />
-            </a>
-          </li>
-          <li>
-            <Link to="/apps" data-metrics-event-name="Sidebar:Link:Mobile">
-              <i className="ss-icon ss-cell vertical-align-middle" />{' '}
-              <LocaleFormattedMessage id="nav.mobile" defaultMessage="Mobile" />
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="/donations"
-              data-metrics-event-name="Sidebar:Link:Contribute"
-            >
-              <i className="ss-icon ss-dollarsign vertical-align-middle" />{' '}
-              <LocaleFormattedMessage
-                id="nav.contribute"
-                defaultMessage="Contribute"
-              />
-            </Link>
-          </li>
-          <li>
-            <a
-              href="https://quran.zendesk.com/hc/en-us/articles/210090626-Development-help"
-              target="_blank"
-              rel="noopener noreferrer"
-              data-metrics-event-name="IndexHeader:Link:Developer"
-            >
-              <i className="ss-icon ss-laptop vertical-align-middle" />{' '}
-              <LocaleFormattedMessage
-                id="nav.developers"
-                defaultMessage="Developers"
-              />
-            </a>
-          </li>
-          <li>
-            <a
-              href="http://legacy.quran.com"
-              data-metrics-event-name="Sidebar:Link:Legacy"
-            >
-              <i className="ss-icon ss-alert vertical-align-middle" />{' '}
-              <LocaleFormattedMessage
-                id="nav.legacySite"
-                defaultMessage="Legacy Quran.com"
-              />
-            </a>
-          </li>
-          <hr />
-          <li>
-            <a
-              href="https://quranicaudio.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              data-metrics-event-name="Sites:Audio"
-            >
-              Audio
-            </a>
-          </li>
-          <li>
-            <a
-              href="http://salah.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              data-metrics-event-name="Sites:Salah"
-            >
-              Salah
-            </a>
-          </li>
-          <li>
-            <a
-              href="http://sunnah.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              data-metrics-event-name="Sites:Sunnah"
-            >
-              Sunnah
-            </a>
-          </li>
-        </List>
-      </div>
-    );
-  }
-}
+    </MenuItem>
+    <MenuItem icon={<Icon type="dollarsign" />} href="/donations">
+      <LocaleFormattedMessage id="nav.contribute" defaultMessage="Contribute" />
+    </MenuItem>
+    <MenuItem
+      icon={<Icon type="laptop" />}
+      href="https://quran.zendesk.com/hc/en-us/articles/210090626-Development-help"
+    >
+      <LocaleFormattedMessage id="nav.developers" defaultMessage="Developers" />
+    </MenuItem>
+    <MenuItem icon={<Icon type="alert" />} href="http://legacy.quran.com">
+      <LocaleFormattedMessage
+        id="nav.legacySite"
+        defaultMessage="Legacy Quran.com"
+      />
+    </MenuItem>
+    <MenuItem divider href="https://quranicaudio.com/">
+      Audio
+    </MenuItem>
+    <MenuItem href="http://salah.com/">
+      Salah
+    </MenuItem>
+    <MenuItem href="http://sunnah.com/">
+      Sunnah
+    </MenuItem>
+  </Menu>
+);
 
 GlobalSidebar.propTypes = {
   settingsModalProps: PropTypes.object, // eslint-disable-line
