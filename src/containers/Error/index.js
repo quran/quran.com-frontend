b64f9df878933210eca7af94e535d2683343c483
hasibsahibzada <hasibullah@sahibzada.org>
2017-10-14 15:20:22 +0200

    prop-types loaded in some other components.

--- src/containers/Error/index.js
+++ src/containers/Error/index.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import IndexHeader from 'components/IndexHeader';
 import Helmet from 'react-helmet';
 import { FormattedHTMLMessage } from 'react-intl';
