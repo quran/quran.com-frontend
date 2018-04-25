3e13aa3f36edcbc1b3d4fc8449911c40d062c405
Mohamed El Mahallawy <mmahalwy@gmail.com>
2018-04-24 23:10:13 -0700

    remove uneeded files

--- shared/components/DemoApp/AsyncAboutRoute/index.js
+++
@@ -1,6 +0,0 @@
-import { asyncComponent } from 'react-async-component';
-
-export default asyncComponent({
-  // include home and about route in same chunk e.g main
-  resolve: () => System.import(/* webpackChunkName: "main" */ './AboutRoute')
-});
--- shared/components/DemoApp/AsyncCounterRoute/index.js
+++
@@ -1,6 +0,0 @@
-import { asyncComponent } from 'react-async-component';
-
-export default asyncComponent({
-  resolve: () =>
-    System.import(/* webpackChunkName: "counter" */ './CounterRoute')
-});
