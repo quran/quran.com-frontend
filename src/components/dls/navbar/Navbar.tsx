ca66718e1cac1204f5d456a12e41289af12bce21
Mohamed El Mahallawy <mmahalwy@gmail.com>
2018-09-03 09:38:10 -0700

    Use new nav

--- src/components/dls/navbar/Navbar.tsx
+++ src/components/dls/navbar/Navbar.tsx
@@ -1,29 +1,28 @@
-import React, { ReactNode } from 'react';
-import PropTypes from 'prop-types';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-const propTypes = {
-  children: PropTypes.node.isRequired,
-};
-
-type Props = {
-  children: ReactNode;
-  scrolled?: boolean;
-};
-
-const NavbarNode = styled.nav`
-  ${({ scrolled }) =>
-    scrolled
-      ? 'box-shadow: 0 4px 5px 0 rgba(0,0,0,0.14), 0 1px 10px 0 rgba(0,0,0,0.12), 0 2px 4px -1px rgba(0,0,0,0.2)'
-      : ''};
+const fixedStyle = css`
+  top: 0;
+  border-width: 0 0 1px;
+  position: fixed;
+  right: 0;
+  left: 0;
+  z-index: 1030;
+  border: 1px solid rgba(0, 0, 0, 0.12);
 `;
 
-const Navbar: React.SFC<Props> = ({ children, scrolled }: Props) => (
-  <NavbarNode scrolled={scrolled}>
-    <div className="container-fluid">{children}</div>
-  </NavbarNode>
-);
+const scrolledCss = css`
+  box-shadow: 0 4px 5px 0 rgba(0, 0, 0, 0.14), 0 1px 10px 0 rgba(0, 0, 0, 0.12),
+    0 2px 4px -1px rgba(0, 0, 0, 0.2);
+  transition: box-shadow 0.5s;
+`;
 
-Navbar.propTypes = propTypes;
+const NavbarNode = styled.nav<{ scrolled?: boolean; fixed?: boolean }>`
+  height: 3.5rem;
+  background: ${({ theme }) => theme.colors.white};
+  font-family: ${({ theme }) => theme.fonts.montserrat};
+  ${({ scrolled }) => (scrolled ? scrolledCss : '')};
 
-export default Navbar;
+  ${({ fixed }) => (fixed ? fixedStyle : '')};
+`;
+
+export default NavbarNode;
